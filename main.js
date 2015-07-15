2957f39b638acaf4ab756b33220e547ee3b77d1c
Robin Burton <1.robinburton@gmail.com>
2015-07-15 18:44:46 -0500

    updates

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var songs = [];
 
-songs[songs.length] = "Legs - by ZZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song - by Supertramp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall - by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welcome to the Jungle - by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c - by Alanis Morisette on the album Jagged Little Pill";
+songs[0] = "Legs - by ZZTop on the album Eliminator";
+songs[1] = "The Logical Song - by Supertramp on the album Breakfast in America";
+songs[2] = "Another Brick in the Wall - by Pink Floyd on the album The Wall";
+songs[3] = "Welcome to the Jungle - by Guns & Roses on the album Appetite for Destruction";
+songs[4] = "Ironi!c - by Alanis Morisette on the album Jagged Little Pill";
 
-songs_unshift("Springstein - by Eric Church on the album Chief");
+songs.unshift("Springstein - by Eric Church on the album Chief");
 
-songs_push("I Love Rock 'n' Roll - by Joan Jett on the album I Love Rock 'n' Roll");
+songs.push("I Love Rock 'n' Roll - by Joan Jett on the album I Love Rock 'n' Roll");
 
-for (var i = 0; < songs; i+= 1) {
+// for (var i = 0; < songs; i+= 1) {
