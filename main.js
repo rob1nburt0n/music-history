2f279f92fa09ac052e3b385093961da735db8a41
Robin Burton <1.robinburton@gmail.com>
2015-07-15 19:27:51 -0500

    completed steps 2 and 3

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 var songs = [];
 
-songs[0] = "Legs - by ZZTop on the album Eliminator";
-songs[1] = "The Logical Song - by Supertramp on the album Breakfast in America";
-songs[2] = "Another Brick in the Wall - by Pink Floyd on the album The Wall";
-songs[3] = "Welcome to the Jungle - by Guns & Roses on the album Appetite for Destruction";
-songs[4] = "Ironi!c - by Alanis Morisette on the album Jagged Little Pill";
+songs[0] = "Legs > by Z*ZTop on the album Eliminator";
+songs[1] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[2] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[3] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[4] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
+/* 
+removals: *, @, (, !
+replacement: remove > and replace with -
+this_song_original_text.replace("*","");
+*/
 songs.unshift("Springstein - by Eric Church on the album Chief");
 
 songs.push("I Love Rock 'n' Roll - by Joan Jett on the album I Love Rock 'n' Roll");
 
-// for (var i = 0; < songs; i+= 1) {
+for (var i = 0; i < songs.length; i++) {
+	console.log(i);
+	var this_song_original_text = songs[i];
+	var new_song_text = this_song_original_text.replace("*","").replace("@","").replace("(","").replace("!","").replace(">","-");
+    console.log(new_song_text);
+};
