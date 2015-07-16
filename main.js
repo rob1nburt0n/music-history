ff1e6a7b916b3780affefa080a054046e828d3b3
Robin Burton <1.robinburton@gmail.com>
2015-07-15 20:56:44 -0500

    completed through step 4

--- main.js
+++ main.js
@@ -10,14 +10,61 @@ songs[4] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 removals: *, @, (, !
 replacement: remove > and replace with -
 this_song_original_text.replace("*","");
+
+wants this format in main content area
+{Song name} by {Artist} on the album {Album}
+
+http://www.javascriptkit.com/javatutors/dom2.shtml  (see reference)
+1. create the TextNode
+var txt = document.createTextNode(" This text was added to the DIV.");
+2. find an element in the DOM, and add the textNode as a child of that.
+document.getElementById('myDiv').appendChild(txt);
+https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+
+txt = document.createElement("p");
+txt.innertHTML = "here's my string";
+var h = document.createElement("H1")                // Create a <h1> element
+var t = document.createTextNode("Hello World");     // Create a text node
+h.appendChild(t);  
+
+
+
+
+THIS CREATES AND ADDS SONG PARAGRAPH TO THE .sidebar__right
+var paragraph = document.createElement("p");
+var theTextNode = document.createTextNode("this is a song name");
+paragraph.appendChild(theTextNode);
+document.getElementsByClassName("sidebar__right")[0].appendChild(paragraph)
+
+
+// if you want to  add to the body: document.body.appendChild(paragraph);
+// if you want to add to the first (zero-th entry) returned with class
+// "sidebar__right" (note the zero below)
+document.getElementsByClassName("sidebar__right")[0].appendChild(paragraph)
+getElementsByClassName returns an array, the appendChild mehtod is 
+only available on elements (e.g. [0]) not on the actual array.
+
+jQuery waiting for document to load $(document).ready...
 */
 songs.unshift("Springstein - by Eric Church on the album Chief");
 
 songs.push("I Love Rock 'n' Roll - by Joan Jett on the album I Love Rock 'n' Roll");
 
+// Clean up the songs with this loop
 for (var i = 0; i < songs.length; i++) {
-	console.log(i);
+	// console.log(i);
 	var this_song_original_text = songs[i];
 	var new_song_text = this_song_original_text.replace("*","").replace("@","").replace("(","").replace("!","").replace(">","-");
-    console.log(new_song_text);
+    // console.log(new_song_text);
+    songs[i] = new_song_text; 
 };
+
+//adding clean songs to the DOM
+
+for (var i = 0; i <songs.length; i++) {
+	var paragraph = document.createElement("p");
+	var theTextNode = document.createTextNode(songs[i]);
+	paragraph.appendChild(theTextNode);
+	// debugger;
+	document.getElementsByClassName("sidebar__right")[0].appendChild(paragraph);
+}
